2800bb26d464ff56d8e0360f5ac0038305df3acc
Tim Strehle <tim.strehle@spiegel.de>
2018-11-12 22:24:07 +0100

    New method DbUtils::generateUuid()

--- src/Utils/DbUtils.php
+++ src/Utils/DbUtils.php
@@ -17,7 +17,7 @@ class DbUtils
             if (strlen($label) === 0) {
                 continue;
             }
-            
+
             $result .= sprintf(':`%s`', $label);
         }
 
@@ -113,5 +113,38 @@ class DbUtils
         }
 
         return $result;
+    }
+
+
+    /**
+     * Generate UUID v4
+     *
+     * @see http://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid/2040279#2040279
+     * @return string
+     */
+    public static function generateUuid(): string
+    {
+        return sprintf
+        (
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
     }
 }
